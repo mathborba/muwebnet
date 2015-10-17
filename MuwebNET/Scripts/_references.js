6b6b2c60110d83e89cab8456a162029f69004a99
matheus.borba <math.borba94@hotmail.com>
2015-10-16 22:01:38 -0300

    Estruturação do layout padrão, criação de arquitetura de arquivos css/js

--- MuwebNET/Scripts/_references.js
+++ MuwebNET/Scripts/_references.js
@@ -3,3 +3,5 @@
 /// <reference path="jquery-1.10.2.js" />
 /// <reference path="bootstrap.js" />
 /// <reference path="respond.js" />
+/// <reference path="core_global.js" />
+/// <reference path="main.min.js" />
---
+++ MuwebNET/Scripts/core_global.js
@@ -0,0 +1,2 @@
+function ask_url(ask,url,target){var detStatus=confirm(ask);if(detStatus){if(target){location.href=url;}else{top.location=url;}}else{return false;}}
+function ask_form(ask,form_name){var detStatus=confirm(ask);if(detStatus){return true;}else{return false;}}
