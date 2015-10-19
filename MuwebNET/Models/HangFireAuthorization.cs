eb386931913d3f208c905aff55dda8590e7d2e8a
Matheus Borba <math.borba94@hotmail.com>
2015-10-19 11:44:29 -0200

    Adição do HangFire para processamento de Jobs assincronos

---
+++ MuwebNET/Models/HangFireAuthorization.cs
@@ -0,0 +1,26 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.Owin;
+using MUwebNET.Web.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MuwebNET
+{
+    public class HangFireAuthorizationFilter : IAuthorizationFilter
+    {
+        public bool Authorize(IDictionary<string, object> owinEnvironment)
+        {
+            bool authorized = false;
+            var user = SessionManager.Current;
+
+            if (user != null)
+            {
+                if (user.Logged && user.CtlCode >= 8)
+                    authorized = true;
+            }
+            return authorized;
+        }
+    }
+}
---
+++ MuwebNET/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Hangfire;
+
+[assembly: OwinStartup(typeof(MuwebNET.Startup))]
+namespace MuwebNET
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("WebDbConnection");
+            app.UseHangfireDashboard("/admin/web-tasks", new DashboardOptions { AuthorizationFilters = new[] { new HangFireAuthorizationFilter() } });
+            app.UseHangfireServer();
+        }
+    }
+}
